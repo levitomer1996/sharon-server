22aa324c2687dcef5e981d3b36224e8d6d771c0a
Tomer Levi <levitomer1996@gmail.com>
2020-05-20 14:25:19 +0300

    for joy

--- src/config/typeorm.config.ts
+++ src/config/typeorm.config.ts
@@ -5,18 +5,34 @@ import * as config from 'config';
 import { Db } from 'typeorm';
 import { Comment } from '../article/comment/comment.entiry';
 
-const DBconfig = config.get('db');
+const dbConfig = config.get('db');
 
+// export const typeOrmConfig: TypeOrmModuleOptions = {
+//   // type: DBconfig.type || 'mysql',
+//   type: 'mysql',
+//   // host: process.env.HOSTNAME || DBconfig.host || 'www.amorcycle.com',
+//   host: 'www.amorcycle.com' || process.env.HOSTNAME,
+//   // port: process.env.PORT || DBconfig.port,
+//   // username: process.env.USERNAME || 'amorcycl_tomerking',
+//   username: 'amorcycl_tomerking' || process.env.username,
+//   // password: process.env.PASSWORD || 'junkerms1',
+//   port: 5000,
+//   password: process.env.password || '5_K~]rNa~ZC[',
+//   // database: process.env.DB_NAME || 'amorcycl_amorcycle',
+
+//   database: 'amorcycl_amorcycle',
+//   logging: true,
+//   entities: [__dirname + '/../**/*.entity.{js,ts}', Comment],
+//   // synchronize: process.env.TYPEORM_SYNC || DBconfig.synchronize,
+//   synchronize: true,
+// };
 export const typeOrmConfig: TypeOrmModuleOptions = {
-  type: DBconfig.type,
-  host: process.env.RDS_HOSTNAME || DBconfig.host,
-  port: process.env.RDS_PORT || DBconfig.port,
-  username: process.env.RDS_USERNAME || 'postgres',
-
-  password: process.env.RDS_PASSWORD || 'junkerms1',
-  database: process.env.RDS_DB_NAME || 'amor-cycle',
-  logging: true,
-  entities: [__dirname + '/../**/*.entity.{js,ts}', Comment],
-  // synchronize: process.env.TYPEORM_SYNC || DBconfig.synchronize,
-  synchronize: true,
+  type: dbConfig.type,
+  host: process.env.HOSTNAME || dbConfig.host,
+  port: process.env.DB_PORT || dbConfig.port || 3306,
+  username: process.env.username || dbConfig.username,
+  password: process.env.password || dbConfig.password,
+  database: process.env.DB_DATABASE || dbConfig.database,
+  entities: [__dirname + '/../**/*.entity.ts', Comment],
+  synchronize: dbConfig.synchronize,
 };
--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import * as config from 'config';
 async function bootstrap() {
   const serverConfig = config.get('server');
   const app = await NestFactory.create(AppModule);
-  const port = process.env.PORT || 5000;
+  const port = process.env.PORT || 8080;
   app.enableCors();
   await app.listen(port);
 }
